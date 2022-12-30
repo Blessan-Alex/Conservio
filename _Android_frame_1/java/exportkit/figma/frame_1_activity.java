29fea8736b381314fc42a5678c0d417a2221bd7f
Blessan-Alex <blazeblessan123@gmail.com>
2022-12-30 17:53:35 +0530

    adding all files

---
+++ _Android_frame_1/java/exportkit/figma/frame_1_activity.java
@@ -0,0 +1,102 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		frame_1
+	 *	@date 		Wednesday 21st of December 2022 01:24:01 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class frame_1_activity extends Activity {
+
+	
+	private View _bg__frame_1_ek2;
+	private View rectangle_1;
+	private View rectangle_2;
+	private TextView add_device;
+	private ImageView rectangle_1__stroke_;
+	private ImageView vector_52__stroke_;
+	private ImageView vector_53__stroke_;
+	private ImageView vector_7;
+	private ImageView vector_8;
+	private ImageView vector_9;
+	private View frame_down;
+	private ImageView ellipse_45;
+	private View ellipse_46;
+	private ImageView vector_10;
+	private ImageView subtract;
+	private View ellipse_65;
+	private ImageView vector_109;
+	private View rectangle_3;
+	private View rectangle_5;
+	private ImageView fan_png;
+	private View rectangle_4;
+	private View rectangle_6;
+	private ImageView cooler_png;
+	private TextView add_your_home_appliances_to_start_saving_the_earth;
+	private TextView conserve_now;
+	private TextView your_devices;
+	private TextView conserve_io;
+	private TextView ceiling_fan;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.frame_1);
+
+		
+		_bg__frame_1_ek2 = (View) findViewById(R.id._bg__frame_1_ek2);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		add_device = (TextView) findViewById(R.id.add_device);
+		rectangle_1__stroke_ = (ImageView) findViewById(R.id.rectangle_1__stroke_);
+		vector_52__stroke_ = (ImageView) findViewById(R.id.vector_52__stroke_);
+		vector_53__stroke_ = (ImageView) findViewById(R.id.vector_53__stroke_);
+		vector_7 = (ImageView) findViewById(R.id.vector_7);
+		vector_8 = (ImageView) findViewById(R.id.vector_8);
+		vector_9 = (ImageView) findViewById(R.id.vector_9);
+		frame_down = (View) findViewById(R.id.frame_down);
+		ellipse_45 = (ImageView) findViewById(R.id.ellipse_45);
+		ellipse_46 = (View) findViewById(R.id.ellipse_46);
+		vector_10 = (ImageView) findViewById(R.id.vector_10);
+		subtract = (ImageView) findViewById(R.id.subtract);
+		ellipse_65 = (View) findViewById(R.id.ellipse_65);
+		vector_109 = (ImageView) findViewById(R.id.vector_109);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		fan_png = (ImageView) findViewById(R.id.fan_png);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		rectangle_6 = (View) findViewById(R.id.rectangle_6);
+		cooler_png = (ImageView) findViewById(R.id.cooler_png);
+		add_your_home_appliances_to_start_saving_the_earth = (TextView) findViewById(R.id.add_your_home_appliances_to_start_saving_the_earth);
+		conserve_now = (TextView) findViewById(R.id.conserve_now);
+		your_devices = (TextView) findViewById(R.id.your_devices);
+		conserve_io = (TextView) findViewById(R.id.conserve_io);
+		ceiling_fan = (TextView) findViewById(R.id.ceiling_fan);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
